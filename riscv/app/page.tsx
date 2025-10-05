a78fc50a12e796a27eebf9bc5e3e23fb86ec400b
angelle10268 <anle16@asu.edu>
2025-10-04 18:19:06 -0700

    UI update (this one looks better)

--- riscv/app/page.tsx
+++ riscv/app/page.tsx
@@ -1,16 +1,32 @@
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-screen space-y-4">
-      <button className="bg-gray-500 text-white px-4 py-2 rounded">
+      <button className="bg-gray-500 text-white px-8 py-2 rounded shadow-md hover:bg-gray-600">
         RUN
       </button>
       <textarea
-        placeholder="code"
-        className="w-1/2 h-40 border border-black-300 rounded p-2"
+        placeholder="Write your code here..."
+        className="w-3/4 h-64 border-2 border-black-400 rounded shadow-md p-3 bg-gray-50 font-mono text-sm"
       ></textarea>
-      <text className="w-1/2 h-40 border border-black-300 rounded p-2">
-        ERROR MESSAGE : NO ERRORS
-      </text>
+      <div className="w-3/4 bg-gray-50 border-2 border-black rounded shadow-md p-4 space-y-3">
+        <p className = "font-semibold text-sm">ERROR MESSAGE : NO ERRORS</p>
+        <div className="flex justify-between text-sm font-mono">
+          {/* Registers Section */}
+          <div>
+            <p className="font-semibold mb-1">REGISTERS</p>
+            <p>x0 : 0xa | 19</p>
+            <p>...</p>
+          </div>
+
+          {/* Memory Section */}
+          <div>
+            <p className="font-semibold mb-1">MEMORY:</p>
+            <p>0x0000: 0xb | 11</p>
+            <p>...</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
