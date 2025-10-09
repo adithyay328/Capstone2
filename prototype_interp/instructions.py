62b4430d9059f3de390c5cd56c43a0eb29010228
Jinn99s <jseo38@asu.edu>
2025-10-08 20:36:55 -0700

    Added SLT and SLTI, still working on unsigned variation

--- prototype_interp/instructions.py
+++ prototype_interp/instructions.py
@@ -352,3 +352,83 @@ class ANDI(Instruction):
 
     # Go return our new ADD instruction
     return cls(*asInts)
+
+class SLT(Instruction):
+    def __init__(self, dIdx, aIdx, bIdx):
+      self.dIdx = dIdx
+      self.aIdx = aIdx
+      self.bIdx = bIdx
+
+    def forward(self, state : MachineState) -> MachineState:
+      """
+      Implements the forward pass of SLT
+      dIdx = aIdx | bIdx
+      """
+      # Get the values of a and b
+      aVal = state.regs[self.aIdx].value
+      bVal = state.regs[self.bIdx].value
+
+      # Add them together, and store in d, with
+      # overflow handling
+      state.regs[self.dIdx].value = 1 if aVal < bVal else 0
+
+      return state
+    
+    @classmethod
+    def parseFromString(cls, s):
+      """
+      The format for this is literally just SLT x0, x1, x2 as a concept
+      """
+      spacedBraked = s.split(" ")
+      assert spacedBraked[0] == "SLT", "Not an SLT instruction"
+
+      # Start by replacing ADD with nothing, and then
+      # trimming
+      args = s.replace("SLT", "").strip()
+
+      # Now, split on commas
+      csv = [int(x.strip()[1:]) for x in args.split(",")]
+
+      # Go return our new ADD instruction
+      return cls(*csv)
+
+class SLTI(Instruction):
+  def __init__(self, dIdx, aIdx, imm):
+    self.dIdx = dIdx
+    self.aIdx = aIdx
+    self.imm = imm
+
+  def forward(self, state : MachineState) -> MachineState:
+    """
+    Implements the forward pass of SLTI
+    dIdx = aIdx ^ bIdx
+    """
+    # Get the values of a and b
+    aVal = state.regs[self.aIdx].value
+    immVal = self.imm
+
+    # Add them together, and store in d, with
+    # overflow handling
+    state.regs[self.dIdx].value = 1 if aVal < immVal else 0
+
+    return state
+
+  @classmethod
+  def parseFromString(cls, s):
+    """
+    The format for this is literally just ADD x0, x1, x2 as a concept
+    """
+    spacedBraked = s.split(" ")
+    assert spacedBraked[0] == "SLTI", "Not an SLTI instruction"
+
+    # Start by replacing ADD with nothing, and then
+    # trimming
+    args = s.replace("SLTI", "").strip()
+
+    # Now, split on commas
+    withoutSpaces = [x.strip() for x in args.split(",")]
+    withoutX = [x.replace("x", "") for x in withoutSpaces]
+    asInts = [int(x) for x in withoutX]
+
+    # Go return our new ADD instruction
+    return cls(*asInts)
