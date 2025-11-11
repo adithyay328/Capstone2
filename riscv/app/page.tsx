eb57d3d46d65bcc9d4739933f5670f16ce4f3889
ethan <ethan@ethans-MacBook-Pro.local>
2025-11-11 12:34:00 -0700

    Added Sidebar Nav for Default Student View

--- riscv/app/page.tsx
+++ riscv/app/page.tsx
@@ -3,13 +3,18 @@ import CodeEditor from "@/components/code-editor";
 import { useState } from "react";
 import AssemblyInfo from "@/components/assembly-info";
 import Root from "@/components/root";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   const [code, setCode] = useState("");
 
   return (
     <div className="flex flex-col justify-center items-center w-screen space-y-4">
-      <Root/>
+      <Sidebar/>
+      <main className="ml-20 md:m2-64 p-4">
+        <Root/>
+      </main>
+      
     </div>
   );
 }
