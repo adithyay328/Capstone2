cc1be27bf2e90b6a31e23c044c0b3461014d22ad
ethan <ethan@ethans-MacBook-Pro.local>
2025-11-25 16:17:46 -0700

    Final Commit for year

--- riscv/app/page.tsx
+++ riscv/app/page.tsx
@@ -3,14 +3,22 @@ import CodeEditor from "@/components/code-editor";
 import { useState } from "react";
 import AssemblyInfo from "@/components/assembly-info";
 import Root from "@/components/root";
+import Sidebar from "@/components/sidebar";
 import { redirect } from 'next/navigation';
 
 
 export default function Home() {
-  // This page should never be reached due to middleware protection
-  // but we'll redirect to login just in case
-  redirect('/login');
+  redirect('/login');  
 
+  return (
+    <div className="flex flex-col justify-center items-center w-screen space-y-4">
+      <Sidebar/>
+      <main className="ml-20 md:m2-64 p-4">
+        <Root/>
+      </main>
+      
+    </div>
+  );
   return (
     <div className="flex flex-col justify-center items-center w-screen space-y-4">
       <Root/>
